a766f46bd02d64333b4d6e96edb016e2952a9511
yoxcu <mick@yoxcu.de>
2023-01-05 19:16:56 +0100

    fix

--- custom_components/rpi_gpio_rotary/sensor.py
+++ custom_components/rpi_gpio_rotary/sensor.py
@@ -54,26 +54,31 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the PWM LED lights."""
-    encoders = []
+    entities = []
     for encoder_conf in config[CONF_ENCODERS]:
         pinA = encoder_conf[CONF_PINA]
         pinB = encoder_conf[CONF_PINB]
+        opt_args = {}
+        opt_args["pin_factory"] = PiGPIOFactory(host=encoder_conf[CONF_HOST], port=encoder_conf[CONF_PORT])
+        encoder = RotaryEncoderSensor(RotaryEncoder(pinA,pinB,**opt_args), "{}_encoder".format(encoder_conf[CONF_NAME]))
+        entities.append(encoder)
+
         pinSW = encoder_conf[CONF_PINSW]
         opt_args = {}
         opt_args["pin_factory"] = PiGPIOFactory(host=encoder_conf[CONF_HOST], port=encoder_conf[CONF_PORT])
-        encoder = RotaryEncoderSensor(RotaryEncoder(pinA,pinB,**opt_args),Button(pinSW,**opt_args), encoder_conf[CONF_NAME])
-        encoders.append(encoder)
+        button = ButtonSensor(Button(pinSW,**opt_args), "{}_button".format(encoder_conf[CONF_NAME]))
+        entities.append(button)
 
-    add_entities(encoders)
+
+    add_entities(entities)
 
 class RotaryEncoderSensor(Entity):
-    def __init__(self, encoder, button, name):
+    def __init__(self, encoder, name):
         super().__init__()
         """Initialize one-color PWM LED."""
         self._name = name
         self._state = 0
         self._available = True
-        self.button = button
         self.encoder = encoder
         self.attrs: Dict[str, Any] = {ATTR_Test: "Test", ATTR_VALUE: 0}
         self.encoder.when_rotated = self.update_Home_Assistant
@@ -104,5 +109,50 @@ class RotaryEncoderSensor(Entity):
     def update_Home_Assistant(self):
         self.attrs[ATTR_VALUE]=self.encoder.value
         self._state = int((self.encoder.value+1)*50)
+        self.async_write_ha_state()
+
+class ButtonSensor(Entity):
+    def __init__(self, button, name):
+        super().__init__()
+        """Initialize one-color PWM LED."""
+        self._name = name
+        self._state = "off"
+        self._available = True
+        self.button = button
+        self.attrs: Dict[str, Any] = {ATTR_Test: "Test", ATTR_VALUE: 0}
+        self.button.when_pressed = self.update_Home_Assistant
+        self.button.when_released = self.update_Home_Assistant
+
+
+    @property
+    def name(self) -> str:
+        """Return the name of the entity."""
+        return self._name
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._available
+
+    @property
+    def state(self) -> Optional[str]:
+        return self._state
+
+    @property
+    def device_state_attributes(self) -> Dict[str, Any]:
+        return self.attrs
+
+    @property
+    def should_poll(self):
+        return False
+    
+    def update_Home_Assistant(self):
+        if self.button.is_pressed:
+            self.attrs[ATTR_VALUE]="on"
+            self._state = "on"
+        else:
+            self.attrs[ATTR_VALUE]="off"
+            self._state = "off"
+
         self.async_write_ha_state()
 
